12a24d2bbcc8799c55bc41f2813a803857966337
PratikG <pratik.gokhale1@gmail.com>
2022-09-21 17:12:06 +0530

    admin site registration

--- backend/node/apps.py
+++ backend/node/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-from .models import Slave
+
 
 class NodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -9,6 +9,7 @@ class NodeConfig(AppConfig):
     def ready(self):
         try:
             from .Coordinator import MASTER
+            from .models import Slave
             for node in MASTER.discover_nodes():
                 print(node._64bit_addr)
                 if not Slave.objects.filter(unique_id = node._64bit_addr).exists():
