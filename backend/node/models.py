560ae56c78058551a72bdedff1ae870a1106adbc
PratikG <pratik.gokhale1@gmail.com>
2022-09-28 11:32:26 +0530

    slave_model changed name

--- backend/node/models.py
+++ backend/node/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Slave(models.Model):
     unique_id = models.CharField(max_length=20,unique=True,primary_key=True)
-    name = models.CharField(max_length=255,unique=True)
+    name = models.CharField(max_length=255,default="Street Light ")
     is_active = models.BooleanField(default=True)
     mains_val = models.BooleanField(default=False)
     dim_val = models.IntegerField(default=25,choices=
