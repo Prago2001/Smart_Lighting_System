d6d337255acfd5c7da6163b3f01585aac32495c8
PratikG <pratik.gokhale1@gmail.com>
2022-12-02 12:17:51 +0530

    v 1.1.7.1 - current values, handle time exp

--- backend/node/Scheduler.py
+++ backend/node/Scheduler.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from astral import LocationInfo
 from astral.sun import sun
 from .models import Schedule, Slot, CurrentMeasurement, TemperatureMeasurement,Slave
+from digi.xbee.exception import TimeoutException
 
 
 try:
@@ -22,37 +23,42 @@ scheduler = BackgroundScheduler()
 
 def getInsValues():
     for node in Slave.objects.all():
-        remote = MASTER.get_node(node.name)
+        try:
+            remote = MASTER.get_node(node.name)
 
-        if remote is None:
-            node.is_active = False
-        else:
-            # If is_active of node is False
-            # and communication re-ocurs then
-            # setting it to current mains value and dim value 
-            if node.is_active is False:
-                remote.set_mains_value(node.mains_val)
-                remote.set_dim_value(node.dim_val)
-            node.is_active = True
-            temp = remote.get_temperature_value() 
-            curr = remote.get_current_value()
-
-            curr = curr - 447
-            if 150 > curr > 50:
-                curr = curr * 2.93
-            elif 250 > curr > 150:
-                curr = curr * 2.44
-            elif 350 > curr > 250:
-                curr = curr * 2.27
+            if remote is None:
+                node.is_active = False
             else:
-                curr = curr * 2.3
-            temp = ((temp * 1.2 / 1023) - 0.5) * 100
-            CurrentMeasurement.objects.create(SlaveId = node,currentValue = curr)
-            TemperatureMeasurement.objects.create(SlaveId = node,temperatureValue = temp)
-            node.current = curr
-            node.temperature = temp
+                # If is_active of node is False
+                # and communication re-ocurs then
+                # setting it to current mains value and dim value
+                if node.is_active is False:
+                    remote.set_mains_value(node.mains_val)
+                    remote.set_dim_value(node.dim_val)
+                node.is_active = True
+                temp = remote.get_temperature_value() 
+                curr = remote.get_current_value()
 
-        node.save()
+                curr = curr - 447
+                if 150 > curr > 50:
+                    curr = curr * 2.93
+                elif 250 > curr > 150:
+                    curr = curr * 2.44
+                elif 350 > curr > 250:
+                    curr = curr * 2.27
+                else:
+                    curr = curr * 2.3
+                temp = ((temp * 1.2 / 1023) - 0.5) * 100
+                CurrentMeasurement.objects.create(SlaveId = node,currentValue = curr)
+                TemperatureMeasurement.objects.create(SlaveId = node,temperatureValue = temp)
+                node.current = curr
+                node.temperature = temp
+
+            node.save()
+        except TimeoutException as e:
+            print(f"Live values from {node.name} couldn't be fetched")
+        except Exception as e:
+            print(f"Error from {node.name}:- ",e,sep="")
 
 def fetchSunModel() :
 
