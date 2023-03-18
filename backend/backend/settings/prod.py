e541a81ec22a784be25721b481d02bf977be5909
PratikG <pratik.gokhale1@gmail.com>
2023-03-18 17:52:36 +0530

    build for production after settings feature

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -79,14 +79,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "pi",
+#         "PASSWORD": "postgres",
+#         "HOST": "localhost",
+#         "PORT": 5432, 
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "pi",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": 5432, 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
