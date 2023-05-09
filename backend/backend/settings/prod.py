01786dadafa4f8db87890534cf50e47385d1de60
PratikG <pratik.gokhale1@gmail.com>
2023-05-09 11:50:50 +0530

    reverting back to postgresql

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "pi",
-#         "PASSWORD": "postgres",
-#         "HOST": "localhost",
-#         "PORT": 5432, 
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 90,
-        }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432, 
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'OPTIONS': {
+#             'timeout': 90,
+#         }
+#     }
+# }
 
 
 # Password validation
